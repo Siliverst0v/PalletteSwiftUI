1365402b28190fd6fc0dd62a39ad4dbd09aed3e2
Anatoly Siliverstov <siliverst0v@yandex.ru>
2022-04-03 21:37:44 +0300

    Initial Commit

---
+++ PalletteSwiftUI/PalletteSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalletteSwiftUIApp.swift
+//  PalletteSwiftUI
+//
+//  Created by Анатолий Силиверстов on 03.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct PalletteSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
